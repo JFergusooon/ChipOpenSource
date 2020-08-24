c0ad86a3e7329dc4cdee8409a24ff8989491822d
19kneumann <kneumann@student.neumont.edu>
2020-08-23 22:35:42 -0600

    added recipe databases, can view all in db, can also view individual ones

--- loginSystem/emptyRecipe.php
+++ loginSystem/emptyRecipe.php
@@ -1,7 +1,28 @@
 <?php
     session_start();
+    require_once '../vendor/autoload.php';
+    $ID = $_GET["recipeID"];
+    $loader = new \Twig\Loader\FileSystemLoader('../templates');
+    $twig = new \Twig\Environment($loader);
+
+    $db = new mysqli('localhost', 'root', '', 'recipesdb');
+
+    if($db->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
+    $sql = <<<SQL
+    SELECT *
+    FROM `publicrecipes`
+    WHERE `recipeID` = $ID
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+    echo $twig->render('recipe.phtml', ['recipe' => $result])
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <title>
@@ -39,4 +60,4 @@
             <p id="developers">Ashton Atkinson, Sawyer Benson, Cory Billing, Danny Doyle, Jeffrey Ferguson, Kayla Neumann <p>
         </footer>
     </body>
-</html>
+</html> -->
--- loginSystem/recipes.php
+++ loginSystem/recipes.php
@@ -1,7 +1,80 @@
 <?php
     session_start();
+    require_once '../vendor/autoload.php';
+
+
+    $loader = new \Twig\Loader\FileSystemLoader('../templates');
+    $twig = new \Twig\Environment($loader);
+
+    //UNCOM TO CREATE DB
+
+    // $connection = new mysqli('localhost', 'root', '');
+    // $sql = "CREATE DATABASE recipesDB";
+    // if (mysqli_query($connection, $sql)) {
+    //   echo "Database created successfully";
+    // } else {
+    //   echo "Error creating database: " . mysqli_error($connection);
+    // }
+    // mysqli_close($connection);
+
+    // $db = new mysqli('localhost', 'root', '', 'recipesDB');
+    // $sql ="CREATE TABLE publicRecipes (
+    //     recipeID INT(255) AUTO_INCREMENT PRIMARY KEY,
+    //     CreatedBy VARCHAR(100),
+    //     Title VARCHAR(200),
+    //     PrepTime VARCHAR(200),
+    //     Ingredients VARCHAR(5000),
+    //     Instructions VARCHAR(5000),
+    //     MiscNotes VARCHAR(5000)
+    //     )";
+    //     if (mysqli_query($db, $sql)) {
+    //         echo "Table created successfully";
+    //       } else {
+    //         echo "Error creating table: " . mysqli_error($db);
+    //       }
+    //       $sql = "INSERT INTO publicrecipes (recipeID, CreatedBy, Title, PrepTime, Ingredients,
+    //       Instructions, MiscNotes)
+    //          VALUES ('2', '19kayla', 'Beef', '100 Minutes', '1 whole cow', 'Kill, and feast', 'make sure you give thanks to the cow')";
+
+    // if (mysqli_query($db, $sql)) {
+    //     echo "New record created successfully";
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    // }
+    // $sql = "INSERT INTO publicrecipes (recipeID, CreatedBy, Title, PrepTime, Ingredients,
+    // Instructions, MiscNotes)
+    //     VALUES ('1', '19kayla', 'Chicken And Rice', '30 Minutes', '4 chicken Breasts, 1 cup rice', 'Cook both the chicken and the rice. Chop the chicken up, and then add to rice', 'ahh')";
+
+    // if (mysqli_query($db, $sql)) {
+    //     echo "New record created successfully";
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    // }
+
+
+    //END CREATE AND INSERT COMMANDS
+
+
+    $db = new mysqli('localhost', 'root', '', 'recipesDB');
+
+    if($db->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
+    $sql = <<<SQL
+    SELECT *
+    FROM `publicrecipes`
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+
+    echo $twig->render('recipes.phtml', ['recipes' => $result])
+
+    
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <title>
@@ -39,4 +112,4 @@
             <p id="developers">Ashton Atkinson, Sawyer Benson, Cory Billing, Danny Doyle, Jeffrey Ferguson, Kayla Neumann <p>
         </footer>
     </body>
-</html>
+</html> -->
